649dad1acd81f4746dc85000e3e97ca9a03696bf
NavdekarIman <imannavdekar08@gmail.com>
2025-03-03 23:39:50 +0530

    Prob No. 2161 - Date: 3-03-2025

--- PartitionArray.java
+++ PartitionArray.java
@@ -1,25 +1,33 @@
+import java.util.Arrays;
+
 class PartitionArray {
     public static void main(String[] args) {
-        
+        Solution solution = new Solution();
+        int[] nums = {1, 5, 11, 20, 6, 7, 3, 2, 6};
+        int pivot = 6;
+
+        // Correct way to print an array
+        System.out.println(Arrays.toString(solution.pivotArray(nums, pivot)));
     }
-    class Solution {
-        public int[] pivotArray(int[] nums, int pivot) {
-            int n = nums.length;
-            int[] result = new int[n];
-            int left = 0, right = n-1;
-            int leftRes = 0, rightRes = n - 1;
-    
-            while (left < n) {
-                if (nums[left] < pivot) 
-                    result[leftRes++] = nums[left];
-                if (nums[right] > pivot)
-                    result[rightRes--] = nums[right];
-                left++;
-                right--;    
-            }
-            while (leftRes <= rightRes)
-                result[leftRes++] = pivot;
-            return result;
+}
+//Soltuion function
+class Solution {
+    public int[] pivotArray(int[] nums, int pivot) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n-1;
+        int leftRes = 0, rightRes = n - 1;
+
+        while (left < n) {
+            if (nums[left] < pivot) 
+                result[leftRes++] = nums[left];
+            if (nums[right] > pivot)
+                result[rightRes--] = nums[right];
+            left++;
+            right--;    
         }
+        while (leftRes <= rightRes)
+            result[leftRes++] = pivot;
+        return result;
     }
 }
