0c61e3f2ae3d8e1b5a1c47d4c421162ab86d0984
NavdekarIman <imannavdekar08@gmail.com>
2025-03-06 21:47:56 +0530

    Prob No. 2579 - Date: 5-03-2025

--- CountTotalNumberOfColoredCells.java
+++ CountTotalNumberOfColoredCells.java
@@ -1,5 +1,11 @@
 class CountTotalNumberOfColoredCells {
     public static void main(String[] args) {
-        
+        Solution solution = new Solution();
+        System.out.println(solution.coloredCells(8));
+    }
+}
+class Solution {
+    public long coloredCells(int n) {
+        return 1 + 4L * n * (n - 1) / 2;
     }
 }
