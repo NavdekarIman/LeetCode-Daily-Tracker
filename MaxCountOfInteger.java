8110efb6840c2889b4206192a06197fa1a01ed12
NavdekarIman <imannavdekar08@gmail.com>
2025-03-12 23:21:43 +0530

    Prob No. 2529 - Date: 12-03-2025

--- MaxCountOfInteger.java
+++ MaxCountOfInteger.java
@@ -1,17 +1,20 @@
 public class MaxCountOfInteger {
-    int[] question = {1, 2, -1, -5, -9, 0, 8, 9};
-    Solution solution = new Solution();
-    System.out.println(solution.maximumCount(question));
+    public static void main(String[] args) { // Added main method
+        int[] question = {1, 2, -1, -5, -9, 0, 8, 9};
+        Solution solution = new Solution();
+        System.out.println(solution.maximumCount(question));
+    }
 }
+
 class Solution {
     public int maximumCount(int[] nums) {
-        int CountOdd = 0, CountEven = 0;
+        int countOdd = 0, countEven = 0; // Fixed variable names
         for (int i = 0; i < nums.length; i++) { 
-            if (nums[i] < 0) CountOdd += 1; 
-            if (nums[i] > 0) CountEven += 1; 
+            if (nums[i] < 0) countOdd += 1; 
+            if (nums[i] > 0) countEven += 1; 
         }
-        int MaximumCountOfNumber = Math.max(CountOdd, CountEven); 
-        System.out.println("The maximum Count is " + MaximumCountOfNumber); 
-        return MaximumCountOfNumber; 
+        int maximumCountOfNumber = Math.max(countOdd, countEven); 
+        System.out.println("The maximum count is " + maximumCountOfNumber); 
+        return maximumCountOfNumber; 
     }
 }
